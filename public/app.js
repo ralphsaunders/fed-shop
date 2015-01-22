7c9f552dd570d744e908bfb06d32f93815d32e3c
Craig Collins <collins.craig@me.com>
2015-01-22 15:31:11 +0000

    fifth commit

---
+++ public/app.js
@@ -0,0 +1,23 @@
+$('.add-suffix-button').on('click', function () {
+	
+	// get reference to text in input
+	var input = $('#suffix-text');
+	var text = input.val();
+	
+	// send post request to API
+	// looks like:
+	// {
+	//  	suffix: 'sausage'
+	// }
+	// logs out response
+	$.post('/api/suffixes', {
+		suffix: text
+	}, function (data) {
+		console.log(data);
+		input.val('')
+	});
+});
+
+$.get('/api', function (response) {
+	$(".random-button").text(response);
+});
